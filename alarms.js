598624d201348bbdaec1336431e4229c520ddd10
Simon Que <sque@chromium.org>
2017-02-21 10:23:12 -0500

    Use new private API

--- alarms.js
+++ alarms.js
@@ -1,3 +1,7 @@
+goog.require('proto.videoconf.gvc.hotrodapi.VideoConfSendDataParams');
+goog.require('proto.videoconf.gvc.hotrodapi.VideoConfSendDataResponse');
+goog.require('proto.videoconf.gvc.hotrodapi.VideoConfSendDataResponse.GetCPUTemperature');
+
 function updateTemperatureField(fieldName, temps) {
   var output = '';
   for (var i = 0; i < temps.length; i++)
@@ -13,6 +17,41 @@ chrome.alarms.onAlarm.addListener(function( alarm ) {
         return;
       updateTemperatureField('temp-old', temps[0]);
     });
+
+    // Set up the protobuf for the API call.
+    var params = new proto.videoconf.gvc.hotrodapi.VideoConfSendDataParams();
+    params.setCommand('GetCPUTemperature');
+    chrome.videoconfPrivate.sendData(
+      new TextDecoder("utf-8").decode(params.serializeBinary()),
+      function(response) {
+        // |response| is an array containing the callback arguments.
+        if (response.length < 2) {
+          console.log("Expected response with 2 elements, but got " +
+                      response.length);
+          return;
+        }
+        var status = response[0];
+        if (status != "SUCCESS") {
+          console.log("Got status " + status);
+          return;
+        }
+        var responseString = response[1];
+        var splitString = responseString.split("");
+        var bytes = new Uint8Array(splitString.length);
+        for (var i = 0, len = bytes.length; i < len; ++i) {
+          bytes[i] = splitString[i].charCodeAt(0);
+          console.log("  Byte: " + bytes[i]);
+        }
+        var response =
+            proto.videoconf.gvc.hotrodapi.VideoConfSendDataResponse.
+                deserializeBinary(bytes);
+        var temp = response.getCpuTemperature();
+        var temp_obj = temp.toObject();
+        var temp2 = temp.getTemperatureList();
+        //var temp = response.getCpuTemperature().getTemperature();
+        console.log(status, temp2);
+      }
+    );
   }
 });
 chrome.alarms.create(alarmName, {delayInMinutes: 0.02, periodInMinutes: 0.02});
