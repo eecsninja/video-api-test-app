23e91e0178cff880bec97bdf4ea389e1c8842b3e
Simon Que <sque@chromium.org>
2017-05-03 12:10:06 -0400

    Change to test incremental logging

--- alarms.js
+++ alarms.js
@@ -1,23 +1,19 @@
 
-function updateTemperatureField(fieldName, temps) {
-  var output = '';
-  for (var i = 0; i < temps.length; i++)
-    output += temps[i].toFixed(3) + '\n';
-  document.getElementById(fieldName).innerText = output;
-}
-
+var readerId = null;
 var alarmName = 'update';
 chrome.alarms.onAlarm.addListener(function( alarm ) {
   if (alarm.name == alarmName) {
-    chrome.system.cpu.getInfo(function(response) {
-      if ('temperatures' in response) {
-        var temps = response['temperatures'];
-        console.log(temps);
-        updateTemperatureField('temp-old', temps);
-      } else {
-        console.log('Could not find temperatures in response: ' + response);
-      }
-    });
+    chrome.feedbackPrivate.readLogSource(
+        "messages",
+        true,
+        readerId,
+        function(id, result) {
+          //console.log(id, result);
+          for (var i = 0, len = result.length; i < len; ++i) {
+            console.log(result[i]);
+          }
+          readerId = id;
+        });
   }
 });
 chrome.alarms.create(alarmName, {delayInMinutes: 0.02, periodInMinutes: 0.02});
